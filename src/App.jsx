a2762a460f93eb1e0279a2923828ba069534322e
davidelucifora <davidelucifora1@gmail.com>
2022-09-16 21:50:23 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { render } from 'react-dom'
+import './App.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1></h1>
+    )
+  }
+}
+
+export default App
